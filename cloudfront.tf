a30e31bf71b9e39b565d671e576c4aaafe22ec56
Jeremiah Norris <jjno91@users.noreply.github.com>
2024-07-01 09:18:19 -0500

    add cache_policy_id

--- cloudfront.tf
+++ cloudfront.tf
@@ -11,6 +11,9 @@ resource "aws_cloudfront_distribution" "this" {
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = "this"
     viewer_protocol_policy = "redirect-to-https"
+
+    # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # CachingOptimized
   }
 
   viewer_certificate {
