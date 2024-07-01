2669f07b31a986b833461a0ccac7777e8109f1e2
Jeremiah Norris <jjno91@users.noreply.github.com>
2024-07-01 09:10:22 -0500

    add restrictions block

--- cloudfront.tf
+++ cloudfront.tf
@@ -16,4 +16,11 @@ resource "aws_cloudfront_distribution" "this" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
 }
